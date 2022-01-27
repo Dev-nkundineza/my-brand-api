f371e0a187a7bdfe23a870afb66718649319654a
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-01-27 14:07:52 +0200

    some added modification

--- src/routes/api/articleRoutes.js
+++ src/routes/api/articleRoutes.js
@@ -1,19 +1,16 @@
-import express from 'express'
+import express from "express";
 
 // import controllers
-import { ArticleController } from '../../controllers/articleController.js'
-const route = express.Router()
-const articleControllers = new ArticleController()
+import { ArticleController } from "../../controllers/articleController.js";
+const route = express.Router();
+const articleControllers = new ArticleController();
 
-
-
-route.post('/', articleControllers.createArticle)
-route.get('/', articleControllers.getAllArticles)
-route.get('/:id', articleControllers.getArticle)
-route.patch('/:id', articleControllers.updateArticle)
-route.delete('/:id', articleControllers.deleteArticle)
+route.post("/", articleControllers.createArticle);
+route.get("/", articleControllers.getAllArticles);
+route.get("/:id", articleControllers.getArticle);
+route.patch("/:id", articleControllers.updateArticle);
+route.delete("/:id", articleControllers.deleteArticle);
 
 // create Article
 
-
-export default route
+export default route;
--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,28 +1,25 @@
-// import the model you need to access
-
-import Article from '../models/article.js'
+import Article from "../models/article.js";
 export class ArticleServices {
     constructor() {}
 
     async createArticle(data) {
-        const article = await Article(data)
-        article.save()
-        return article
+        const article = await Article(data);
+        article.save();
+        return article;
     }
     async getAllArticles() {
-        const article = await Article.find()
+        const article = await Article.find();
         return article;
     }
     async getArticle(id) {
-        const singleArticle =await Article.findOne({_id:id})
+        const singleArticle = await Article.findOne({ _id: id });
         return singleArticle;
     }
     async updateArticle(id) {
-        const singleArticle =await Article.findOne({_id:id})
+        const singleArticle = await Article.findOne({ _id: id });
         return singleArticle;
     }
-   async deleteArticle(id) {
-        await Article.deleteOne({_id:id})
-        
+    async deleteArticle(id) {
+        await Article.deleteOne({ _id: id });
     }
 }
