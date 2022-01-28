f9890ed7bd4e24dd42756f3a3d4fc0dd19ea2308
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-01-28 12:29:51 +0200

    user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,73 @@
-import { UserServices } from "../services/userServices.js"
+import { UserServices } from "../services/userServices.js";
 export class userController {
-
     async createProfile(req, res, next) {
-
         const data = {
-            "username": req.body.username,
-            "password": req.body.password
+            username: req.body.username,
+            password: req.body.password,
+        };
+        const saveUser = await new UserServices().createUser(data);
+        res.status(200).json({
+            status: 200,
+            message: "user created successfully",
+            data: saveUser,
+        });
+    }
+
+    async _getAllUsers(req, res, next) {
+        try {
+            const allUsers = await new UserServices().getUser();
+            res
+                .status(200)
+                .json({ status: 200, message: "you get all users", data: allUsers });
+        } catch (error) {
+            console.log(error);
         }
-        const saveUser = await new UserServices().createUser(data)
-        res.status(200).json({ status: 200, message: "user created successfully", data: saveUser })
+    }
+
+    // GET SINGLE USER
+
+    async _getOneUsers(req, res, next) {
+        try {
+            const user = await new UserServices().getOneUser(req.params.id);
+            res
+                .status(200)
+                .json({ status: 200, message: "selected user", data: user });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // update an user
+
+    async _updateUser(req, res, next) {
+        try {
+            const user = await new UserServices().updateProfile(req.params.id);
+            if (req.body.username) {
+                user.username = req.body.username;
+            }
+            if (req.body.password) {
+                user.password = req.body.password;
+            }
+
+            res.status(200).json({
+                status: 200,
+                message: "profile has been updated",
+                data: user,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // DELETE USER
+
+    async _deleteUser(req, res, next) {
+
+        await new UserServices().deleteUser(req.params.id)
+        res.status(200).json({
+            status: 200,
+            message: `user ${req.params.id} has been deleted`,
+        })
+
     }
 }
--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -6,5 +6,9 @@ const userControllels = new userController()
 const route = express.Router()
 
 route.post('/', userControllels.createProfile)
+route.get('/', userControllels._getAllUsers)
+route.get('/:id', userControllels._getOneUsers)
+route.patch('/:id', userControllels._updateUser)
+route.delete('/:id', userControllels._deleteUser)
 
 export default route
