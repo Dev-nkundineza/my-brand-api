065a539b6c32137d5160f76a38c24892aaba0d59
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-01-28 12:29:51 +0200

    user profile

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,20 +1,55 @@
-import Profile from "../models/user.js"
+import Profile from "../models/user.js";
 
 export class UserServices {
     async createUser(data) {
-        try {
-            const profile = await Profile(data)
-            profile.save()
+            try {
+                const profile = await Profile(data);
+                profile.save();
 
-            return profile
+                return profile;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        // get all users
+    async getUser() {
+        try {
+            const allUsers = await Profile.find();
+            console.log(allUsers);
+            return allUsers;
 
         } catch (error) {
             console.log(error);
         }
 
     }
-    getUser() {}
-    getUser(id) {}
-    updateProfile(id) {}
-    deleteUser(id) {}
+
+    // get one user
+    async getOneUser(id) {
+            try {
+                const user = await Profile.findOne({ _id: id });
+                return user;
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        // update profile
+
+    async updateProfile(id) {
+        try {
+            const getItem = await Profile.findOne({ _id: id });
+            return getItem;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+
+    //delete user
+    async deleteUser(id) {
+        await Profile.deleteOne({ _id: id });
+    }
 }
