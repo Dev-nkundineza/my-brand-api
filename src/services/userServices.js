8eb21920dc67aa9ccfa61245ff023476e6f2bc30
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-01-31 04:18:46 +0200

    creating user

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,54 +1,16 @@
-import Profile from "../models/user.js";
-export class UserServices {
-    async createUser(data) {
-            try {
-                const profile = await Profile(data);
-                profile.save();
+import Account from "../models/user.js";
 
-                return profile;
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        // get all users
-    async getUser() {
-        try {
-            const allUsers = await Profile.find();
-            console.log(allUsers);
-            return allUsers;
-
-        } catch (error) {
-            console.log(error);
-        }
-
+export const userExist = async(email) => {
+    const user = await Account.findOne({ email: email });
+    if (user) {
+        return user;
+    } else {
+        return false;
     }
+};
 
-    // get one user
-    async getOneUser(id) {
-            try {
-                const user = await Profile.findOne({ _id: id });
-                return user;
-
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        // update profile
-
-    async updateProfile(id) {
-        try {
-            const getItem = await Profile.findOne({ _id: id });
-            return getItem;
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-
-
-    //delete user
-    async deleteUser(id) {
-        await Profile.deleteOne({ _id: id });
-    }
-}
+export const createUser = async(user) => {
+    const userCreated = await Account(user);
+    userCreated.save();
+    return userCreated;
+};
