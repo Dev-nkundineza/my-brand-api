17bafde4ab2107d1a2d78e1a2822d747302331c2
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-01-31 05:17:50 +0200

    working on user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,78 +1,100 @@
-import { UserServices } from "../services/userServices.js"
-import bcrypt from "bcryptjs"
+// import { UserServices } from "../services/userServices.js"
+import { generateToken } from "../../helpers/jwtFunctions.js"
+import { comparePassword, hashPassword } from "../../helpers/passwordSecurity.js"
+import { userExist, createUser } from "../services/userServices.js"
+import { fileUpload } from "../../helpers/fileUpload.js"
+
+
 export class userController {
     async createProfile(req, res, next) {
 
-
-        const hashed = await bcrypt.hash(req.body.password, 12)
-
-        const data = {
-            username: req.body.username,
-            password: hashed,
-        };
-        const saveUser = await new UserServices().createUser(data);
-        res.status(200).json({
-            status: 200,
-            message: "user created successfully",
-            data: saveUser,
-        });
-    }
-
-    async _getAllUsers(req, res, next) {
         try {
-            const allUsers = await new UserServices().getUser();
-            res
-                .status(200)
-                .json({ status: 200, message: "you get all users", data: allUsers });
+
+            const exist = await userExist(req.body.email)
+            if (exist) {
+                res.status(409).json({
+                    status: 409,
+                    message: `this email ${req.body.email} exists`
+                })
+            } else {
+                if (req.file) {
+                    req.body.picture = await fileUpload(req)
+                } else {
+                    req.body.picture = 'https://www.kindpng.com/imgv/iThJmoo_white-gray-circle-avatar-png-transparent-png/'
+                }
+
+                const user = {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: await hashPassword(req.body.password),
+                    picture: req.body.picture,
+                }
+
+                const createdUser = await createUser(user)
+                res.status(201).json({ status: 201, message: "user registered successfully", user: createdUser })
+            }
+
         } catch (error) {
             console.log(error);
         }
+
     }
+
+    // async _getAllUsers(req, res, next) {
+    //     try {
+    //         const allUsers = await new UserServices().getUser();
+    //         res
+    //             .status(200)
+    //             .json({ status: 200, message: "you get all users", data: allUsers });
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     // GET SINGLE USER
 
-    async _getOneUsers(req, res, next) {
-        try {
-            const user = await new UserServices().getOneUser(req.params.id);
-            res
-                .status(200)
-                .json({ status: 200, message: "selected user", data: user });
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    // async _getOneUsers(req, res, next) {
+    //     try {
+    //         const user = await new UserServices().getOneUser(req.params.id);
+    //         res
+    //             .status(200)
+    //             .json({ status: 200, message: "selected user", data: user });
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     // update an user
 
-    async _updateUser(req, res, next) {
-        try {
-            const user = await new UserServices().updateProfile(req.params.id);
-            if (req.body.username) {
-                user.username = req.body.username;
-            }
-            if (req.body.password) {
-                user.password = req.body.password;
-            }
+    // async _updateUser(req, res, next) {
+    //     try {
+    //         const user = await new UserServices().updateProfile(req.params.id);
+    //         if (req.body.username) {
+    //             user.username = req.body.username;
+    //         }
+    //         if (req.body.password) {
+    //             user.password = req.body.password;
+    //         }
 
-            res.status(200).json({
-                status: 200,
-                message: "profile has been updated",
-                data: user,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    //         res.status(200).json({
+    //             status: 200,
+    //             message: "profile has been updated",
+    //             data: user,
+    //         });
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-    // DELETE USER
+    // // DELETE USER
 
-    async _deleteUser(req, res, next) {
+    // async _deleteUser(req, res, next) {
 
-        await new UserServices().deleteUser(req.params.id)
-        res.status(200).json({
-            status: 200,
-            message: `user ${req.params.id} has been deleted`,
-        })
+    //     await new UserServices().deleteUser(req.params.id)
+    //     res.status(200).json({
+    //         status: 200,
+    //         message: `user ${req.params.id} has been deleted`,
+    //     })
 
-    }
+    // }
 }
--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -1,14 +1,18 @@
 import express from 'express'
+import multer from 'multer'
+import { fileFilter } from '../../../helpers/fileFilter.js'
+
 import { userController } from '../../controllers/userController.js'
 
 const userControllels = new userController()
-
+const storage = multer.diskStorage({});
+const uploads = multer({ storage, fileFilter })
 const route = express.Router()
 
-route.post('/', userControllels.createProfile)
-route.get('/', userControllels._getAllUsers)
-route.get('/:id', userControllels._getOneUsers)
-route.patch('/:id', userControllels._updateUser)
-route.delete('/:id', userControllels._deleteUser)
+route.post('/register', uploads.single('picture'), userControllels.createProfile)
+    // route.get('/', userControllels._getAllUsers)
+    // route.get('/:id', userControllels._getOneUsers)
+    // route.patch('/:id', userControllels._updateUser)
+    // route.delete('/:id', userControllels._deleteUser)
 
 export default route
