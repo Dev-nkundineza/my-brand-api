c93d2d2add2c30ad61c2a840f89f18b7bba3a269
Dev-nkundineza <niyonzimadeus2002@gmail.com>
2022-02-04 08:21:50 +0200

    minor changes on imageupload

--- helpers/imageUpload.js
+++ helpers/imageUpload.js
@@ -1,9 +1,9 @@
 import cloudinary from "cloudinary";
 import "dotenv/config";
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_USER_NAME.toString(),
-    api_key: process.env.CLOUDINARY_API_KEY.toString(),
-    api_secret: process.env.CLOUDINARY_API_SECRET.toString(),
+    cloud_name: process.env.CLOUDINARY_USER_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 console.log(process.env.CLOUDINARY_USER_NAME);
